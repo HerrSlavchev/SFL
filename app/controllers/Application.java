c0b06606cdd12d41722d95272511e78d7d85a638
Stefan <stef2georg@gmail.com>
2014-11-16 17:45:37 +0200

    Json in controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import views.html.*;
+import play.mvc.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		JsonNode json = request().body().asJson();
+		if (json == null) {
+			return badRequest("Expecting Json data");
+		} else {
+			String name = json.findPath("name").textValue();
+			if (name == null) {
+				return badRequest("Missing parameter [name]");
+			} else {
+				return ok("Hello " + name);
+			}
+		}
+	}
 
 }
